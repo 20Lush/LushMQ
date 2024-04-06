307b748ce9e7975b7ac23708197157456f889370
ZACHARY JOHNSON <zachfsx83@gmail.com>
2024-04-05 17:10:13 -0700

    [LushMQ] rich comments

--- library/include/LushMQ.hpp
+++ library/include/LushMQ.hpp
@@ -4,15 +4,44 @@
 // Make sure to change the namespace LIBRARY to something more specific
 namespace lush::lushmq {
 
+	/**	@brief Versioning information from CMake build indicating major version number
+	 * 	@returns compile time major version integer
+	*/
 	constexpr int LMQ_VERS_MAJOR();
+
+	/**	@brief Versioning information from CMake build indicating minor version number
+	 * 	@returns compile time minor version integer
+	*/
 	constexpr int LMQ_VERS_MINOR();
+
+	/**	@brief Versioning information from CMake build indicating patch version number
+	 * 	@returns compile time patch version integer
+	*/
 	constexpr int LMQ_VERS_PATCH();
 
+	/**	@brief Inhereted ZeroMQ major version number
+	 * 	@returns integer called from cppzmq tuple 
+	*/
 	int ZMQ_VERS_MAJOR();
+
+	/**	@brief Inhereted ZeroMQ minor version number
+	 * 	@returns integer called from cppzmq tuple 
+	*/
 	int ZMQ_VERS_MINOR();
+
+	/**	@brief Inhereted ZeroMQ patch version number
+	 * 	@returns integer called from cppzmq tuple 
+	*/
 	int ZMQ_VERS_PATCH();
 
+	/**	@brief LushMQ version number in string form
+	 * 	@returns string in "major.minor.patch" format
+	*/
 	std::string LushMqVersion();
+
+	/**	@brief Inhereted ZeroMQ version number in string form
+	 * 	@returns string in "major.minor.patch" format
+	*/
 	std::string ZeroMqVersion();
 
 }  // namespace lush::lushmq
--- library/src/LushMQ.cpp
+++ library/src/LushMQ.cpp
@@ -1,8 +1,7 @@
 #include "LushMQ.hpp"
+#include "version.hpp"
 
 #include <zmq.hpp>
-
-#include "version.hpp"
 
 namespace lush::lushmq {
 
